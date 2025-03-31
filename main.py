1643e9b1abc262d5274922b5a22154ae73b96b81
Marius Kleiner <kleiner@gmail.com>
2025-03-31 20:43:24 +0200

    Add support to import playlists from M3U files

--- main.py
+++ main.py
@@ -40,7 +40,12 @@ def parse_filename(filename):
     base_name = os.path.splitext(filename.strip())[0]
     parts = base_name.replace("_", " ").split(" - ")
     if len(parts) == 2:
+        debug("Filename has artist and title")
         artist, title = parts
+        return artist, title
+    elif len(parts) == 3 and parts[0].isdigit():
+        debug("Filename has track no., artist, and title.")
+        _, artist, title = parts
         return artist, title
     return None, None
 
@@ -103,7 +108,7 @@ def add_tracks_to_playlist(sp, playlist_id, track_uris):
                 sp.playlist_add_items(playlist_id, batch)
 
 
-def list_mp3_files(directory: str):
+def mp3_files_from_directory(directory: str):
     """
     Returns a list of all *.mp3 or *.MP3 files (case insensitive) in the given directory.
     The list contains only filenames, not full paths.
@@ -114,11 +119,35 @@ def list_mp3_files(directory: str):
     return [f for f in os.listdir(directory) if f.lower().endswith(".mp3")]
 
 
+def mp3_files_from_m3u(file: str):
+    """
+    Returns a list of MP3 file names scanned from an M3U playlist.
+    """
+    mp3_files = []
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+    except UnicodeDecodeError:
+        debug("Unicode decoding error, falling back to iso-8859-1.")
+        with open(file, "r", encoding="iso-8859-1") as f:
+            lines = f.readlines()
+
+    for line in lines:
+        line = line.strip()
+        if not line.startswith("#") and line.endswith(".mp3"):
+            # Basename because sometimes M3U files can contain full or
+            # relative paths. We also need to ensure the native path separator
+            # is used.
+            mp3_files.append(os.path.basename(line.replace("\\", os.sep)))
+    return mp3_files
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Create a Spotify playlist from a list of songs."
     )
-    parser.add_argument("--dir", required=True, help="Name of the directory to scan.")
+    parser.add_argument("--dir", required=False, help="Name of the directory to scan.")
+    parser.add_argument("--m3u", required=False, help="M3U file to scan.")
     parser.add_argument(
         "--playlist", required=True, help="Name of the Spotify playlist to create."
     )
@@ -129,8 +158,16 @@ def main():
     global verbose
     verbose = args.verbose
 
-    if not os.path.isdir(args.dir):
-        print("Error: {directory} is not a valid directory.")
+    if (not args.dir and not args.m3u) or (args.dir and args.m3u):
+        print("Error: you must specify exactly one of --dir or --m3u.")
+        sys.exit(1)
+
+    if args.dir and not os.path.isdir(args.dir):
+        print(f"Error: {args.dir} is not a valid directory.")
+        sys.exit(1)
+
+    if args.m3u and not (os.path.exists(args.m3u) and os.access(args.m3u, os.R_OK)):
+        print(f"File {args.m3u} could not be read.")
         sys.exit(1)
 
     SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET = load_credentials()
@@ -153,25 +190,36 @@ def main():
     )
 
     user_id = sp.current_user()["id"]
-    track_uris = []
     unparsed_files = []
     unmatched_songs = []
 
-    for line in list_mp3_files(args.dir):
+    files = []
+    if args.dir:
+        files = mp3_files_from_directory(args.dir)
+    elif args.m3u:
+        files = mp3_files_from_m3u(args.m3u)
+    else:
+        raise RuntimeError("Invariant error: should be one of two args.")
+
+    # Pre-initialize list so that we can preserve the order of the input. This
+    # matters less for directories but is important for playlists.
+    track_uris = [None] * len(files)
+
+    for i, line in enumerate(files):
         filename = line.strip()
         artist, title = parse_filename(filename)
         if artist and title:
             track_uri = search_track(sp, artist, title)
             if track_uri:
-                track_uris.append(track_uri)
+                track_uris[i] = track_uri
             else:
-                unmatched_songs.append((filename, artist, title))
+                unmatched_songs.append((i, filename, artist, title))
         else:
             unparsed_files.append(filename)
 
     still_unmatched_songs = []
     if unmatched_songs:
-        for filename, artist, title in unmatched_songs:
+        for i, filename, artist, title in unmatched_songs:
             print(f"No exact match for {filename}. Using fuzzy search.")
             query = f"{artist} {title}"
             while True:
@@ -183,7 +231,7 @@ def main():
                     "Select a match number, enter alternative search string, or press Enter to skip: "
                 )
                 if choice.isdigit() and 1 <= int(choice) <= len(fuzzy_matches):
-                    track_uris.append(fuzzy_matches[int(choice) - 1][0])
+                    track_uris[i] = fuzzy_matches[int(choice) - 1][0]
                     break
                 elif choice:
                     query = choice
@@ -194,6 +242,9 @@ def main():
     playlist_name = args.playlist
     playlist_id = get_or_create_playlist(sp, user_id, playlist_name)
     print(f"Playlist created: {playlist_name}")
+
+    # Remove positions without any matches
+    track_uris = [uri for uri in track_uris if uri]
 
     add_tracks_to_playlist(sp, playlist_id, track_uris)
 
